8fb615c5edecaca4afb8ca11904f6cda3ce16859
nguyenphanlongvu <vuvt6019@gmail.com>
2023-10-23 22:49:34 +0700

    update file

---
+++ src/Ex_layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-dark p-5 text-white mt-3">
+          <p>Copyright © Your Website 2023</p>
+        </div>
+      </div>
+    );
+  }
+}
